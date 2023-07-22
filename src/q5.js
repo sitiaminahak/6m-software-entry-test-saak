72805498e3759666cb842774cc990bbaf6f7f102
Siti Aminah <sitiaminahabdulkarim9@gmail.com>
2023-07-22 10:50:42 +0800

    Update q5.js

--- src/q5.js
+++ src/q5.js
@@ -6,6 +6,11 @@
 */
 
 // Task 1: Add code here
-
+import print from "./external.js";
 // Do not change the code below
 document.querySelector("#test").innerHTML = print();
+
+/* comment for q5, based on the W3 note, Modules only work with the HTTP(s) protocol. I can't test this using file://
+A web-page opened via the file:// protocol cannot use import / export. 
+For document.querySelector("#test").innerHTML = print();, is this a typo error? Should be "test" instead of "#test"? Thanks!
+*/
